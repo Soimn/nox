6ab1cf1af6b80198bfe52d3f9ae41cca3c96a67a
Soimn <soimn.dok@gmail.com>
2023-07-26 00:57:57 +0200

    lexer update

--- src/x_lexer.h
+++ src/x_lexer.h
@@ -2,36 +2,39 @@
 
 #define X_TOKEN_KIND__BASS_TO_B(K) ((K) & 0x7F)
 #define X_TOKEN_KIND__B_TO_BASS(K) ((K) | 0x80)
+#define X_TOKEN_KIND__BASS_BIT_IDX 7
 
 typedef enum X_Token_Kind
 {
   /// BLOCK 0
   X_Token_Invalid = 0,
   X_Token_EndOfFile,
-  X_Token_OpenParen,                           // (
-  X_Token_CloseParen,                          // )
-  X_Token_OpenBracket,                         // [
-  X_Token_CloseBracket,                        // ]
-  X_Token_OpenBrace,                           // {
-  X_Token_CloseBrace,                          // }
-  X_Token_Period,                              // .
-  X_Token_PeriodParen,                         // .(
-  X_Token_PeriodBracket,                       // .[
-  X_Token_PeriodBrace,                         // .{
-  X_Token_Cash,                                // $
-  X_Token_Colon,                               // :
-  X_Token_Comma,                               // ,
-  X_Token_Semicolon,                           // ;
+  X_Token_OpenParen,                             // (
+  X_Token_CloseParen,                            // )
+  X_Token_OpenBracket,                           // [
+  X_Token_CloseBracket,                          // ]
+  X_Token_OpenBrace,                             // {
+  X_Token_CloseBrace,                            // }
+  X_Token_Cash,                                  // $
+  X_Token_Colon,                                 // :
+  X_Token_Comma,                                 // ,
+  X_Token_Semicolon,                             // ;
+  X_Token_Arrow,                                 // ->
+  X_Token_Qmark,                                 // ?
+  X_Token_TripleMinus,                           // ---
+  X_Token_Not,                                   // !
   X_Token__PastLastBlock0,
 
   /// BLOCK 1
   X_Token__FirstBlock1 = X_TOKEN_KIND__BLOCK(1),
-  X_Token_BitNot = X_Token__FirstBlock1,       // ~
-  X_Token_Hat,                                 // ^
-  X_Token_Arrow,                               // ->
-  X_Token_Qmark,                               // ?
-  X_Token_TripleMinus,                         // ---
-  X_Token_Not,                                 // !
+  X_Token_BitNot = X_Token__FirstBlock1,         // ~
+  X_Token_Hat,                                   // ^
+  X_Token_Period,                                // .
+  X_Token_Elipsis,                               // ..
+  X_Token_ElipsisLess,                           // ..<
+  X_Token_PeriodParen,                           // .(
+  X_Token_PeriodBracket,                         // .[
+  X_Token_PeriodBrace,                           // .{
   X_Token_Identifier,
   X_Token_String,
   X_Token_Int,
@@ -41,30 +44,30 @@ typedef enum X_Token_Kind
   /// BLOCK 2
   X_Token__FirstMulLevel = X_TOKEN_KIND__BLOCK(2),
   X_Token_Star = X_Token__FirstMulLevel,         // *
-  X_Token_Slash,                               // /
-  X_Token_Rem,                                 // %
-  X_Token_BitAnd,                              // &
-  X_Token_BitShr,                              // >>
-  X_Token_BitSar,                              // >>>
-  X_Token_BitShl,                              // <<
+  X_Token_Slash,                                 // /
+  X_Token_Rem,                                   // %
+  X_Token_BitAnd,                                // &
+  X_Token_BitShr,                                // >>
+  X_Token_BitSar,                                // >>>
+  X_Token_BitShl,                                // <<
   X_Token__PastLastMulLevel,
 
   /// BLOCK 3
   X_Token__FirstAddLevel = X_TOKEN_KIND__BLOCK(3),
   X_Token_Plus = X_Token__FirstAddLevel,         // +
-  X_Token_Minus,                               // -
-  X_Token_BitOr,                               // |
-  X_Token_BitXor,                              // ~
+  X_Token_Minus,                                 // -
+  X_Token_BitOr,                                 // |
+  X_Token_BitXor,                                // ~
   X_Token__PastLastAddLevel,
 
   /// BLOCK 4
   X_Token__FirstCmpLevel = X_TOKEN_KIND__BLOCK(4),
   X_Token_EqualsEQ = X_Token__FirstCmpLevel,     // ==
-  X_Token_NotEQ,                               // !=
-  X_Token_Less,                                // <
-  X_Token_LessEQ,                              // <=
-  X_Token_Greater,                             // >
-  X_Token_GreaterEQ,                           // >=
+  X_Token_NotEQ,                                 // !=
+  X_Token_Less,                                  // <
+  X_Token_LessEQ,                                // <=
+  X_Token_Greater,                               // >
+  X_Token_GreaterEQ,                             // >=
   X_Token__PastLastCmpLevel,
 
   /// BLOCK 5
@@ -100,20 +103,20 @@ typedef enum X_Token_Kind
   /// BLOCK 10
   X_Token__FirstMulLevelAssign = X_TOKEN_KIND__BLOCK(8 + 2),
   X_Token_StarEQ = X_Token__FirstMulLevelAssign, // *=
-  X_Token_SlashEQ,                             // /=
-  X_Token_RemEQ,                               // %=
-  X_Token_BitAndEQ,                            // &=
-  X_Token_BitShrEQ,                            // >>=
-  X_Token_BitSarEQ,                            // >>>=
-  X_Token_BitShlEQ,                            // <<=
+  X_Token_SlashEQ,                               // /=
+  X_Token_RemEQ,                                 // %=
+  X_Token_BitAndEQ,                              // &=
+  X_Token_BitShrEQ,                              // >>=
+  X_Token_BitSarEQ,                              // >>>=
+  X_Token_BitShlEQ,                              // <<=
   X_Token__PastLastMulLevelAssign,
 
   /// BLOCK 11
   X_Token__FirstAddLevelAssign = X_TOKEN_KIND__BLOCK(8 + 3),
   X_Token_PlusEQ = X_Token__FirstAddLevelAssign, // +=
-  X_Token_MinusEQ,                             // -=
-  X_Token_BitOrEQ,                             // |=
-  X_Token_BitXorEQ,                            // ^=
+  X_Token_MinusEQ,                               // -=
+  X_Token_BitOrEQ,                               // |=
+  X_Token_BitXorEQ,                              // ^=
   X_Token__PastLastAddLevelAssign,
 
   /// BLOCK 12
@@ -163,7 +166,8 @@ typedef struct X_Lexer
   X_String input;
 
   X_Text_Pos init_pos;
-  X_u32 offset_to_line;
+  X_u32 init_offset_to_line;
+  X_u32 line_cursor;
   X_u32 line;
 } X_Lexer;
 
@@ -171,12 +175,31 @@ X_Lexer
 X_Lexer_Init(X_String input, X_Text_Pos pos, X_u32 offset_to_line, X_u32 line)
 {
   return (X_Lexer){
-    .cursor         = 0,
-    .input          = input,
-    .init_pos       = pos,
-    .offset_to_line = offset_to_line,
-    .line           = line,
+    .cursor              = 0,
+    .input               = input,
+    .init_pos            = pos,
+    .init_offset_to_line = offset_to_line,
+    .line_cursor         = 0,
+    .line                = line,
   };
+}
+
+X_bool
+X_Lexer__IsAlpha(X_u8 c)
+{
+  return ((X_u8)((c&0xDF) - 'A') <= (X_u8)('Z' - 'A'));
+}
+
+X_bool
+X_Lexer__IsDigit(X_u8 c)
+{
+  return ((X_u8)(c - '0') < 10);
+}
+
+X_bool
+X_Lexer__IsHexAlphaDigit(X_u8 c)
+{
+  return ((X_u8)((c&0xDF) - 'A') <= (X_u8)('F' - 'A'));
 }
 
 X_Token
@@ -186,14 +209,183 @@ X_Lexer_NextToken(X_Lexer* lexer)
 
   for (;;)
   {
+    X_u8 c  = (lexer->cursor   < lexer->input.size ? lexer->input.data[lexer->cursor]   : 0);
+    X_u8 c1 = (lexer->cursor+1 < lexer->input.size ? lexer->input.data[lexer->cursor+1] : 0);
+    if (c == '\n')
+    {
+      lexer->cursor += 1;
+
+      lexer->line_cursor = (X_u32)lexer->cursor;
+      lexer->line       += 1;
+    }
+    else if (c == ' '  || c == '\t' ||
+             c == '\v' || c == '\f' ||
+             c == '\r')
+    {
+      lexer->cursor += 1;
+    }
+    else if (c == '/' && c1 == '/')
+    {
+      lexer->cursor += 2;
+      while (lexer->cursor < lexer->input.size && *lexer->input.data != '\n') lexer->cursor += 1;
+    }
+    else if (c == '/' && c1 == '*')
+    {
+      lexer->cursor += 2;
+      for (X_uint nesting = 1; nesting != 0;)
+      {
+        if (lexer->cursor + 1 >= lexer->input.size)
+        {
+          //// ERROR: Unterminated block comment
+          X_NOT_IMPLEMENTED;
+          break;
+        }
+        else
+        {
+          c  = lexer->input.data[lexer->cursor];
+          lexer->cursor += 1;
+          c1 = lexer->input.data[lexer->cursor];
+          if      (c == '/' && c1 == '*') lexer->cursor += 1, nesting += 1;
+          else if (c == '*' && c1 == '/') lexer->cursor += 1, nesting -= 1;
+          else if (c == '\n')
+          {
+            lexer->line_cursor = (X_u32)lexer->cursor;
+            lexer->line       += 1;
+          }
+        }
+      }
+    }
+    else break;
   }
 
-  token.text = (X_Text){
-    .pos  = ,
-    .size = ,
-  };
+  X_ASSERT(lexer->cursor <= X_U32_MAX - lexer->init_pos);
+  token.text.pos       = (X_u32)(lexer->init_pos + lexer->cursor);
+  // NOTE: token.text.size is set at the end
+  token.offset_to_line = lexer->init_offset_to_line + lexer->line_cursor;
+  token.line           = lexer->line;
+
+  if (lexer->cursor >= lexer->input.size) token.kind = X_Token_EndOfFile;
+  else
+  {
+    X_u8 c[4] = { lexer->input.data[lexer->cursor] };
+    lexer->cursor += 1;
+
+    X_uint headroom = lexer->input.size - lexer->cursor;
+    if (headroom > 0) c[1] = lexer->input.data[lexer->cursor];
+    if (headroom > 1) c[2] = lexer->input.data[lexer->cursor+1];
+    if (headroom > 2) c[3] = lexer->input.data[lexer->cursor+2];
+
+    X_u8 c1_eq_adv = (c[1] == '=' ? 1 : 0);
+    X_u8 c1_eq_bit = c1_eq_adv << X_TOKEN_KIND__BASS_BIT_IDX;
+    X_u8 c2_eq_adv = (c[1] == '=' ? 2 : 1);
+    X_u8 c2_eq_bit = (c2_eq_adv-1) << X_TOKEN_KIND__BASS_BIT_IDX;
+    switch (c[0])
+    {
+      case '(': token.kind = X_Token_OpenParen;    break;
+      case ')': token.kind = X_Token_CloseParen;   break;
+      case '[': token.kind = X_Token_OpenBracket;  break;
+      case ']': token.kind = X_Token_CloseBracket; break;
+      case '{': token.kind = X_Token_OpenBrace;    break;
+      case '}': token.kind = X_Token_CloseBrace;   break;
+      case '$': token.kind = X_Token_Cash;         break;
+      case ':': token.kind = X_Token_Colon;        break;
+      case ',': token.kind = X_Token_Comma;        break;
+      case ';': token.kind = X_Token_Semicolon;    break;
+      case '^': token.kind = X_Token_Hat;          break;
+      case '?': token.kind = X_Token_Qmark;        break;
+
+      case '*': lexer->cursor += c1_eq_adv, token.kind = c1_eq_bit | X_Token_Star;   break;
+      case '/': lexer->cursor += c1_eq_adv, token.kind = c1_eq_bit | X_Token_Slash;  break;
+      case '%': lexer->cursor += c1_eq_adv, token.kind = c1_eq_bit | X_Token_Rem;    break;
+      case '+': lexer->cursor += c1_eq_adv, token.kind = c1_eq_bit | X_Token_Plus;   break;
+      case '~': lexer->cursor += c1_eq_adv, token.kind = c1_eq_bit | X_Token_BitXor; break;
+      case '=': lexer->cursor += c1_eq_adv, token.kind = c1_eq_bit | X_Token_Equals; break;
+      case '!': lexer->cursor += c1_eq_adv, token.kind = c1_eq_bit | X_Token_Not;    break;
+
+      case '-':
+      {
+        if      (c[1] == '>')                lexer->cursor += 1, token.kind = X_Token_Arrow;
+        else if (c[1] == '-' && c[2] == '-') lexer->cursor += 2, token.kind = X_Token_TripleMinus;
+        else lexer->cursor += c1_eq_adv, token.kind = c1_eq_bit | X_Token_Minus;
+      } break;
+
+      case '&':
+      {
+        if (c[1] == '&') lexer->cursor += c2_eq_adv, token.kind = c2_eq_bit | X_Token_And;
+        else             lexer->cursor += c1_eq_adv, token.kind = c1_eq_bit | X_Token_BitAnd;
+      } break;
+
+      case '|':
+      {
+        if (c[1] == '|') lexer->cursor += c2_eq_adv, token.kind = c2_eq_bit | X_Token_Or;
+        else             lexer->cursor += c1_eq_adv, token.kind = c1_eq_bit | X_Token_BitOr;
+      } break;
+
+      case '<':
+      {
+        if (c[1] == '<') lexer->cursor += c2_eq_adv, token.kind = c2_eq_bit | X_Token_BitShl;
+        else             lexer->cursor += c1_eq_adv, token.kind = c1_eq_bit | X_Token_Less;
+      } break;
+
+      case '>':
+      {
+        if      (c[1] == '>' && c[2] == '>' && c[3] == '=') lexer->cursor += 3, token.kind = X_Token_BitSarEQ;
+        else if (c[1] == '>' && c[2] == '>')                lexer->cursor += 2, token.kind = X_Token_BitSar;
+        else if (c[1] == '>') lexer->cursor += c2_eq_adv, token.kind = c2_eq_bit | X_Token_BitShr;
+        else                  lexer->cursor += c1_eq_adv, token.kind = c1_eq_bit | X_Token_Greater;
+      } break;
+
+      case '.':
+      {
+        if      (c[1] == '.' && c[2] == '<') lexer->cursor += 2, token.kind = X_Token_ElipsisLess;
+        else if (c[1] == '.')                lexer->cursor += 1, token.kind = X_Token_Elipsis;
+        else if (c[1] == '(')                lexer->cursor += 1, token.kind = X_Token_PeriodParen;
+        else if (c[1] == '[')                lexer->cursor += 1, token.kind = X_Token_PeriodBracket;
+        else if (c[1] == '}')                lexer->cursor += 1, token.kind = X_Token_PeriodBrace;
+        else                                                     token.kind = X_Token_Period;
+      } break;
+
+      default:
+      {
+        if (c[0] == '_' || X_Lexer__IsAlpha(c[1]))
+        {
+          X_String identifier = { .data = lexer->input.data + lexer->cursor };
+
+          for (;;)
+          {
+            if (lexer->cursor >= lexer->input.size) break;
+
+            X_u8 ci = lexer->input.data[lexer->cursor];
+            if (ci != '_' || !X_Lexer__IsDigit(ci) || !X_Lexer__IsAlpha(ci)) break;
+            else                                                             lexer->cursor += 1;
+          }
+
+          identifier.size = (X_u32)(lexer->cursor + lexer->input.data - identifier.data);
+
+          // TODO: Keywords and interning?
+          X_NOT_IMPLEMENTED;
+        }
+        else if (X_Lexer__IsDigit(c[1]))
+        {
+          X_NOT_IMPLEMENTED;
+        }
+        else if (c[0] == '"')
+        {
+          X_NOT_IMPLEMENTED;
+        }
+        else
+        {
+          //// ERROR: Unknown symbol
+        }
+      } break;
+    }
+  }
+
+  token.text.size = (X_u32)(lexer->cursor - token.text.pos); // TODO: on error
+
   return token;
 }
 
 #undef X_TOKEN_KIND__B_TO_BASS
 #undef X_TOKEN_KIND__BASS_TO_B
+#undef X_TOKEN_KIND__BASS_BIT_IDX
